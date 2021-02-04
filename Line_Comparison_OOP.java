57247d69e4d2d6140dd43ec1f6d8e40cd756a211
pravin47 <bagul.pravin2@gmail.com>
2021-02-05 01:02:29 +0530

    Added:Calculate_Lenght_Of_Line

---
+++ Line_Comparison_OOP.java
@@ -0,0 +1,28 @@
+package com.Line_compa;
+
+import java.util.*;
+
+class calculation {
+
+	public static void lenght_of_line() {
+		int x1, x2, y1, y2;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("\n Enter the x1 and y1 co-ordinates");
+		x1 = sc.nextInt();
+		y1 = sc.nextInt();
+		System.out.println("\n Enter the x2 and y2 co-ordinates");
+		x2 = sc.nextInt();
+		y2 = sc.nextInt();
+		double Lenght_Of_Line = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+		System.out.println("lenght of Line is :" + Lenght_Of_Line);
+	}
+}
+
+public class Line_Comparison_oop extends calculation {
+
+	public static void main(String args[]) {
+		System.out.println("WELCOME TO LINE COMPARISON PROGRAM");
+		lenght_of_line();
+	}
+
+}
